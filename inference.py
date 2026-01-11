07cc308a5f38c635b44dc237ef26e889505d6a2f
Nakhwan Maulana <130434857+itsnakhwanm@users.noreply.github.com>
2026-01-11 16:58:26 +0700

    Add files via upload

--- inference.py
+++ inference.py
@@ -11,10 +11,9 @@ def parse_args():
     parser = argparse.ArgumentParser(description="A tool to create readable caption for accessibility purposes.",epilog="example")
     parser.add_argument("filename", help='A media filename. Could be either video (with audio) or audio.')
     parser.add_argument("-o", "--output", type=str, required=True)
-    parser.add_argument("--max-words", type=int, default=7, help="Max words")
-    parser.add_argument("--max-lines", type=int, default=2)
+    parser.add_argument("--max-words", type=int, default=7, help="Max words.\nWarning: If you type 2, it will inevitably create single-word captions.")
     parser.add_argument("--model-size", type=str, default="small", help="Model size.")
-    parser.add_argument("--device", type=str, default="cpu", help="Device used to render.")
+    parser.add_argument("--device", type=str, default="cpu", help="Device used to transcribe.")
     args = parser.parse_args()
     return args
     print(args.filename)
@@ -24,15 +23,22 @@ def main(args):
     result = model.transcribe(args.filename,word_timestamps=True,verbose=True)
 
     words=get_word_timestamps(result)
+    print(words)
     texts=get_text(result)
-    texts=split_by_word_limit(texts)
-    aligned=align_segments_to_words(texts,words)
+    texts=split_by_word_limit(texts,max_words=args.max_words)
+    print(texts)
+    aligned=align_independent(texts,words)
+    print(aligned)
 
-    write_srt(text, args.output)
+    output_splitted = os.path.splitext(args.output)
+    if output_splitted[1] == ".srt":
+        write_srt(aligned, args.output)
+    elif output_splitted[1] == ".vtt":
+        write_vtt(aligned, args.output)
 
 if __name__ == "__main__":
     args = parse_args()
-    if args.max_words == 2 and args.max_lines == 1:
+    if args.max_words == 2:
         if warning_sign("Caution: This will inevitably create single-word captions.\nASIF cannot condone misuse of this option.\nDo you want to continue?"):
             main(args)
     else:
